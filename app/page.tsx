83178b9830769b5c48926eb2d37307e0d05ba05e
tjdeveloper-work <tjmr1996@gmail.com>
2026-01-16 17:27:06 +0500

    feat: make homepage dynamic

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+export const dynamic = 'force-dynamic';
 import { Suspense } from 'react';
 import { AppSidebar } from '@/components/app-sidebar';
 import { SidebarInset } from '@/components/ui/sidebar';
